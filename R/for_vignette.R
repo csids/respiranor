8424c1a43b6d9a17d758192831925d7ddb460ddf
chizhang <andreachizhang@yahoo.com>
2023-05-18 14:07:31 +0200

    test changes

---
+++ R/for_vignette.R
@@ -0,0 +1,20 @@
+# this script is to store some development code for vignette
+
+
+# total
+
+
+# originally the data had
+
+
+# try to get weekly, nation, cases
+
+# case_test <- total[granularity_time == 'isoyearweek' &
+#                      granularity_geo == 'nation',
+#                    .(cases_by_testdate_n)]
+#
+#
+# plot(case_test$cases_by_testdate_n)
+
+
+
